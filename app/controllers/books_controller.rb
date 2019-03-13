2670c74d1556b92baf39cccae2eea89c20dc621f
yusa0730 <isizawa5226@gmail.com>
2019-03-13 16:16:02 +0900

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def index
+  	  @book = Book.new
+  	  @books = Book.all
+  end
+
+
+  def create
+  	  book = Book.new(book_params)
+  	  if book.save
+        flash[:notice] = "Book was successfully created."
+  	  redirect_to book_path(book.id)
+    end
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+  def update
+      book = Book.find(params[:id])
+      if book.update(book_params)
+        flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+      end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_index_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root :to => 'books#top'
+  get 'books/top'
+  get 'books/index'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_post'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  post 'books' => 'books#create'
+
+  delete 'books/:id' =>'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
